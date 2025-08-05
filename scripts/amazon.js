3c768f1d9c9f866fac8a95c70a5f62ca41ef7529
Soham Wagale <sohamwagale@gmail.com>
2025-08-05 20:52:39 +0530

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        price: 1090 
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: 'Intemediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        price: 2095
-    },
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        price: 799 
-    }
-];
-
 let prodHTML = '';
 products.forEach((product)=>{
     prodHTML += `
@@ -50,7 +20,7 @@ products.forEach((product)=>{
             </div>
 
             <div class="product-price">
-                $${(product.price /100).toFixed()}
+                $${(product.price /100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -75,10 +45,37 @@ products.forEach((product)=>{
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = '${product.id}'>
                 Add to Cart
             </button>
         </div>`
 });
 const productsElement = document.querySelector(".js-products-grid");
 productsElement.innerHTML = prodHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+        button.addEventListener('click',()=>{
+            const productId = button.dataset.productId;//the kebab case product-name is converted to camel case productName
+
+            let mathcingItem;
+
+            cart.forEach((item)=>{
+                if(item.productId = productId){
+                    mathcingItem = item;
+                }
+            })
+
+            if(mathcingItem){
+                mathcingItem.quantity +=1;
+            }
+            else{
+                cart.push({
+                    productId:productId,
+                    quantity:1
+                });
+            }
+
+            console.log(cart);
+        })
+    })
