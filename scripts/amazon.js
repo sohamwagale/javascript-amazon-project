5092d1b107a3dbccde4af3bc22ebe5ed5289c80e
Soham Wagale <sohamwagale@gmail.com>
2025-08-06 23:25:15 +0530

    Import Products form products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 let prodHTML = '';
 products.forEach((product)=>{
     prodHTML += `
@@ -24,7 +26,7 @@ products.forEach((product)=>{
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class = "js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -40,7 +42,7 @@ products.forEach((product)=>{
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
@@ -53,7 +55,10 @@ products.forEach((product)=>{
 const productsElement = document.querySelector(".js-products-grid");
 productsElement.innerHTML = prodHTML;
 
-document.querySelectorAll('.js-add-to-cart')
+
+const addedMessageTimeouts = {};
+
+document.querySelectorAll('.js-add-to-cart')//using for each because we wanna add event listener to all buttons
     .forEach((button)=>{
         button.addEventListener('click',()=>{
             const productId = button.dataset.productId;//the kebab case product-name is converted to camel case productName
@@ -66,16 +71,44 @@ document.querySelectorAll('.js-add-to-cart')
                 }
             })
 
+            const selectedQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+            console.log(selectedQuantity)
             if(mathcingItem){
-                mathcingItem.quantity +=1;
+                mathcingItem.quantity +=selectedQuantity;
             }
             else{
                 cart.push({
                     productId:productId,
-                    quantity:1
+                    quantity: selectedQuantity
                 });
             }
 
-            console.log(cart);
+            const addedDisplay = document.querySelector(`.js-added-to-cart-${productId}`);
+            
+            addedDisplay.classList.add('added-to-cart-visible');
+
+            const previousTimeoutId = addedMessageTimeouts[productId];
+            //hya productId che kahi previous timeouts asle tar te clear karayche
+            if(previousTimeoutId){
+                clearTimeout(previousTimeoutId)
+            }
+
+            const addedDisplayTimeoutId =setTimeout(()=>{
+                addedDisplay.classList.remove('added-to-cart-visible');
+            },2000);
+
+            addedMessageTimeouts[productId] = addedDisplayTimeoutId;
+            //nantar tyach productId la navin timeoutId assign karaycha override karun
+
+            let cartTotalQuantity = 0;
+            cart.forEach((item)=>{
+                cartTotalQuantity += item.quantity;
+            })
+
+            document.querySelector('.js-cart-total-quantity')
+                .innerHTML = cartTotalQuantity;
+
+            console.log(cartTotalQuantity);
+            //console.log(cart.q)
         })
     })
